c67a45c4e64239393eecec82fefa9c825f0d0b23
Akash Agravat <79508409+akashagravat@users.noreply.github.com>
2022-01-18 15:40:52 +0530

    First Commit
    Add javascript/jquery for make payment and your details

--- HTML/js/BookService.js
+++ HTML/js/BookService.js
@@ -1,82 +1,175 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $("#selectbed").select2();
-   $("#selectbath").select2();
-   $("#selectime").select2();
-   $("#selecthour").select2();
+    $("#selectbath").select2();
+    $("#selectime").select2();
+    $("#selecthour").select2();
 
 
-   $('#selectdate').datepicker({
-format:'dd-mm-yyyy'
-});
+    $('#selectdate').datepicker({
+        format: 'dd-mm-yyyy'
+    });
 
-$('.first').click(function(e){
-e.preventDefault();
-$("#firstservice").attr('src',"image/3-green.png");
-$(".first").css({
-"border": "3px solid #1D7A8C"
-});
-$(".first").dblclick(function(){
-$("#firstservice").attr('src',"image/3.png");
-$(".first").css({
-"border": "1px solid #C8C8C8"
-});
-});
-});
-
-$('.second').click(function(e){
-e.preventDefault();
-$("#secondimg").attr('src',"image/5-green.png");
-$(".second").css({
-"border": "3px solid #1D7A8C"
-});
-$(".second").dblclick(function(){
-$("#secondimg").attr('src',"image/5.png");
-$(".second").css({
-"border": "1px solid #C8C8C8"
-});
-});
-});
-
-$('.third').click(function(e){
-e.preventDefault();
-$("#thirdimg").attr('src',"image/4-green.png");
-$(".third").css({
-"border": "3px solid #1D7A8C"
-});
-$(".third").dblclick(function(){
-$("#thirdimg").attr('src',"image/4.png");
-$(".third").css({
-"border": "1px solid #C8C8C8"
-});
-});
-});
-
-$('.fourth').click(function(e){
-e.preventDefault();
-$("#fourthimg").attr('src',"image/2-green.png");
-$(".fourth").css({
-"border": "3px solid #1D7A8C"
-});
-$(".fourth").dblclick(function(){
-$("#fourthimg").attr('src',"image/2.png");
-$(".fourth").css({
-"border": "1px solid #C8C8C8"
-});
-});
-});
-
-$('.fifth').click(function(e){
-e.preventDefault();
-$("#fifthimg").attr('src',"image/1-green.png");
-$(".fifth").css({
-"border": "3px solid #1D7A8C"
-});
-$(".fifth").dblclick(function(){
-$("#fifthimg").attr('src',"image/1.png");
-$(".fifth").css({
-"border": "1px solid #C8C8C8"
-});
-});
-});
 
 });
+
+// Change Image On click
+$(document).ready(function(){
+    $(".first").click(function() {
+        chngimg1();
+    });
+    $(".second").click(function(){
+        chngimg2()
+    });
+    $(".third").click(function(){
+        chngimg3()
+    });
+    $(".fourth").click(function(){
+        chngimg4()
+    });
+    $(".fifth").click(function(){
+        chngimg5()
+    });
+    var toggle = false;
+    
+    function chngimg1() {
+        if (toggle == true) {
+            $(".first").css({
+                "border": "3px solid #1D7A8C"
+            });
+            document.getElementById('firstservice').src  = 'image/3-green.png';
+        } else {
+           document.getElementById('firstservice').src = 'image/3.png';
+           $(".first").css({
+            "border": "1px solid #C8C8C8"
+                });
+        }
+        toggle = !toggle; 
+    }
+    function chngimg2() {
+        if (toggle == true) {
+            $(".second").css({
+                "border": "3px solid #1D7A8C"
+            });
+            document.getElementById('secondimg').src  = 'image/5-green.png';
+        } else {
+           document.getElementById('secondimg').src = 'image/5.png';
+           $(".second").css({
+            "border": "1px solid #C8C8C8"
+                });
+        }
+        toggle = !toggle; 
+    }
+    function chngimg3() {
+        if (toggle == true) {
+            $(".third").css({
+                "border": "3px solid #1D7A8C"
+            });
+            document.getElementById('thirdimg').src  = 'image/4-green.png';
+        } else {
+           document.getElementById('thirdimg').src = 'image/4.png';
+           $(".third").css({
+            "border": "1px solid #C8C8C8"
+                });
+        }
+        toggle = !toggle; 
+    }
+    function chngimg4() {
+        if (toggle == true) {
+            $(".fourth").css({
+                "border": "3px solid #1D7A8C"
+            });
+            document.getElementById('fourthimg').src  = 'image/2-green.png';
+        } else {
+           document.getElementById('fourthimg').src = 'image/2.png';
+           $(".fourth").css({
+            "border": "1px solid #C8C8C8"
+                });
+        }
+        toggle = !toggle; 
+    }
+    function chngimg5() {
+        if (toggle == true) {
+            $(".fifth").css({
+                "border": "3px solid #1D7A8C"
+            });
+            document.getElementById('fifthimg').src  = 'image/1-green.png';
+        } else {
+           document.getElementById('fifthimg').src = 'image/1.png';
+           $(".fifth").css({
+            "border": "1px solid #C8C8C8"
+                });
+        }
+        toggle = !toggle; 
+    }
+})
+
+// Radio Button
+
+$(document).ready(function () {
+    $('input:radio').change(function () {//Clicking input radio
+        var radioClicked = $(this).attr('id');
+        unclickRadio();
+        removeActive();
+        clickRadio(radioClicked);
+        makeActive(radioClicked);
+    });
+    $(".addresses").click(function () {//Clicking the card
+        var inputElement = $(this).find('input[type=radio]').attr('id');
+        unclickRadio();
+        removeActive();
+        makeActive(inputElement);
+        clickRadio(inputElement);
+    });
+});
+
+
+function unclickRadio() {
+    $("input:radio").prop("checked", false);
+}
+
+function clickRadio(inputElement) {
+    $("#" + inputElement).prop("checked", true);
+}
+
+function removeActive() {
+    $(".addresses").removeClass("active");
+}
+
+function makeActive(element) {
+    $("#" + element + "-card").addClass("active");
+}
+
+// Card Format
+$(document).ready(function () {
+
+    //For Card Number formatted input
+    
+    $('#cr_no').keyup(function() {
+        var crno = $(this).val().split(" ").join(""); // remove hyphens
+        if (crno.length > 0) {
+            crno = crno.match(new RegExp('.{1,4}', 'g')).join(" ");
+        }
+        $(this).val(crno);
+      });
+
+    //For Date formatted input
+    $('#exp').keyup(function() {
+        var crnos = $(this).val().split("/").join(""); // remove hyphens
+        if (crnos.length > 0) {
+            crnos = crnos.match(new RegExp('.{1,2}', 'g')).join("/");
+        }
+        $(this).val(crnos);
+      });
+});
+
+// Checkbox checked-unchecked when click paragraph
+
+$('.final-submits').bind('click',function(){
+    var input = $(this).find('input');  
+    if(input.prop('checked')){
+      input.prop('checked',false);
+    }else{
+      input.prop('checked',true);
+    }
+  });
+  
